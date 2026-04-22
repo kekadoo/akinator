4585da1e40e48c2154191649e5391958aaceec57
e-ryzhkova <e.ryzhkova@g.nsu.ru>
2026-04-22 20:56:25 +0700

    Update io.h

--- src/io.h
+++ src/io.h
@@ -1,1 +1,30 @@
+#ifndef IO_H
+#define IO_H
 
+typedef struct IO_interface IO_interface; // скрытая структура
+
+// Создать объект ввода/вывода (можно хранить файл лога и т.п.)
+IO_interface* io_create();
+
+// Задать вопрос и получить ответ (1 - да, 0 - нет, -1 - ошибка/выход)
+int ask_question(IO_interface *io, const char *question);
+
+// Вывести сообщение о догадке, вернуть подтверждение (1 - да, 0 - нет)
+int confirm_object(IO_interface *io, const char *object_name);
+
+// Запросить правильный объект (строка выделяется malloc, caller free)
+char* ask_real_object(IO_interface *io, const char *wrong_object);
+
+// Запросить новый вопрос
+char* ask_new_question(IO_interface *io, const char *correct_object, const char *wrong_object);
+
+// Запросить ответ на новый вопрос для правильного объекта
+int ask_answer_for_new(IO_interface *io, const char *question, const char *correct_object);
+
+// Вывести сообщение об ошибке
+void io_error(IO_interface *io, const char *msg);
+
+// Освободить IO_interface
+void io_free(IO_interface *io);
+
+#endif
